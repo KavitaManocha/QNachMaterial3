fca0bf894c3002437e19fe1d88a59d1b053ec763
Kavita Manocha <kavita28978@gmail.com>
2022-08-30 15:42:14 +0530

    added reports id to action bar and added selector for navigation menu.

--- app/src/main/java/com/example/qnachlocal/DashboardActivity.kt
+++ app/src/main/java/com/example/qnachlocal/DashboardActivity.kt
@@ -28,11 +28,12 @@ class DashBoardActivity : AppCompatActivity() {
         navView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_dash_board)
+        navView!!.itemIconTintList = null
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_reports,R.id.navigation_notifications
             )
         )
 //        setupActionBarWithNavController(navController, appBarConfiguration)
