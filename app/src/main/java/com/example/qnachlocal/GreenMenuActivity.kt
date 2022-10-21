c937602b06cea5ee526543bacd74963642129eb3
Kavita Manocha <kavita28978@gmail.com>
2022-10-21 14:26:41 +0530

    implemented dialog popup on checking the last item in backstack.

--- app/src/main/java/com/example/qnachlocal/GreenMenuActivity.kt
+++ app/src/main/java/com/example/qnachlocal/GreenMenuActivity.kt
@@ -1,11 +1,11 @@
 package com.example.qnachlocal
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import br.com.leandroferreira.wizard_forms.contract.WizardNavigator
 import com.example.qnachlocal.databinding.ActivityGreenMenuBinding
-import com.example.qnachlocal.databinding.ActivityMainBinding
-import com.example.qnachlocal.ui.components.greenmenuflow.personaldetails.PersonalDetailsFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,6 +16,34 @@ class GreenMenuActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityGreenMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onBackPressed() {
+        if (fragmentManager.backStackEntryCount != 0) {
+            fragmentManager.popBackStack()
+        } else {
+            val builder = AlertDialog.Builder(this)
+            //set title for alert dialog
+            builder.setTitle(R.string.dialogTitle)
+            //set message for alert dialog
+            builder.setMessage(R.string.message)
+
+            //performing positive action
+            builder.setPositiveButton("Yes"){dialogInterface, which ->
+                val intent = Intent(this,DashBoardActivity::class.java)
+                startActivity(intent)
+            }
+            //performing negative action
+            builder.setNegativeButton("Cancel"){dialogInterface, which ->
+                val intent = Intent(this,GreenMenuActivity::class.java)
+                startActivity(intent)
+            }
+            // Create the AlertDialog
+            val alertDialog: AlertDialog = builder.create()
+            // Set other dialog properties
+            alertDialog.setCancelable(false)
+            alertDialog.show()
+        }
     }
 }
 
