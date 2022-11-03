41165ca16feb48c8b9bd5464bb2106425266916e
Kavita Manocha <kavita28978@gmail.com>
2022-11-03 16:07:48 +0530

    Made the app run only in light mode.

--- app/src/main/java/com/example/qnachlocal/SplashActivity.kt
+++ app/src/main/java/com/example/qnachlocal/SplashActivity.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.qnachlocal.data.local.SessionManager
 import com.google.android.material.textview.MaterialTextView
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_splash)
         val appVersion = findViewById<MaterialTextView>(R.id.appVersion)
         appVersion.setText(BuildConfig.VERSION_NAME)
