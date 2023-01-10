49276b3e900442bc36aed4f94a27e7c84956e79f
Kavita Manocha <kavita28978@gmail.com>
2023-01-10 17:13:37 +0530

    implemented stepper for forms

--- app/src/main/java/com/example/qnachlocal/GreenMenuActivity.kt
+++ app/src/main/java/com/example/qnachlocal/GreenMenuActivity.kt
@@ -2,6 +2,7 @@ package com.example.qnachlocal
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -9,40 +10,121 @@ import com.example.qnachlocal.databinding.ActivityGreenMenuBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class GreenMenuActivity: AppCompatActivity() {
+class GreenMenuActivity : AppCompatActivity() {
     private lateinit var binding: ActivityGreenMenuBinding
+    private var position = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGreenMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.stepView.done(false)
+
+        binding.btnNext.setOnClickListener {
+            when (position) {
+                0 -> {
+                    binding.personalDetails.visibility = View.GONE
+                    binding.accountDetails.visibility = View.VISIBLE
+                    position = 1
+                    binding.stepView.done(false)
+                    binding.stepView.go(position, true)
+                    binding.btnNext.text = "NEXT"
+                    binding.btnBack.visibility = View.VISIBLE
+                    binding.btnBack.text = "BACK"
+                }
+                1 -> {
+                    binding.accountDetails.visibility = View.GONE
+                    binding.mandateDetails.visibility = View.VISIBLE
+                    position = 2
+                    binding.stepView.done(false)
+                    binding.stepView.go(position, true)
+                    binding.btnNext.text = "NACH GENERATE"
+                    binding.btnBack.visibility = View.VISIBLE
+                    binding.btnBack.text = "BACK"
+
+                }
+                else -> {
+                    binding.mandateDetails.visibility = View.GONE
+                    binding.personalDetails.visibility = View.VISIBLE
+                    position = 0
+                    binding.stepView.done(false)
+                    binding.stepView.go(position, true)
+                    binding.btnNext.text = "NEXT"
+                    binding.btnBack.visibility = View.GONE
+                }
+            }
+        }
+
+        binding.btnBack.setOnClickListener{
+            when(position){
+                1->{
+                    binding.personalDetails.visibility = View.VISIBLE
+                    binding.accountDetails.visibility = View.GONE
+                    position = 0
+                    binding.stepView.done(false)
+                    binding.stepView.go(position, true)
+                    binding.btnNext.text = "Next"
+                    binding.btnBack.visibility = View.GONE
+                }
+                2 -> {
+                    binding.accountDetails.visibility = View.VISIBLE
+                    binding.mandateDetails.visibility = View.GONE
+                    position = 1
+                    binding.stepView.done(false)
+                    binding.stepView.go(position, true)
+                    binding.btnNext.text = "Next"
+                    binding.btnBack.text = "Back"
+                }
+            }
+        }
     }
 
-    override fun onBackPressed() {
-        if (fragmentManager.backStackEntryCount != 0) {
-            fragmentManager.popBackStack()
-        } else {
-            val builder = AlertDialog.Builder(this)
-            //set title for alert dialog
-            builder.setTitle(R.string.dialogTitle)
-            //set message for alert dialog
-            builder.setMessage(R.string.message)
 
-            //performing positive action
-            builder.setPositiveButton("Yes"){dialogInterface, which ->
-                val intent = Intent(this,DashBoardActivity::class.java)
-                startActivity(intent)
+    override fun onBackPressed() {
+        when (position) {
+            0 -> {
+                val builder = AlertDialog.Builder(this)
+                //set title for alert dialog
+                builder.setTitle(R.string.dialogTitle)
+                //set message for alert dialog
+                builder.setMessage(R.string.message)
+
+                //performing positive action
+                builder.setPositiveButton("Yes") { dialogInterface, which ->
+                    val intent = Intent(this, DashBoardActivity::class.java)
+                    startActivity(intent)
+                }
+                //performing negative action
+                builder.setNegativeButton("Cancel") { dialogInterface, which ->
+                    val intent = Intent(this, GreenMenuActivity::class.java)
+                    startActivity(intent)
+                }
+                // Create the AlertDialog
+                val alertDialog: AlertDialog = builder.create()
+                // Set other dialog properties
+                alertDialog.setCancelable(false)
+                alertDialog.show()
             }
-            //performing negative action
-            builder.setNegativeButton("Cancel"){dialogInterface, which ->
-                val intent = Intent(this,GreenMenuActivity::class.java)
-                startActivity(intent)
+            1 -> {
+                binding.personalDetails.visibility = View.VISIBLE
+                binding.accountDetails.visibility = View.GONE
+                position = 0
+                binding.stepView.done(false)
+                binding.stepView.go(position, true)
+                binding.btnNext.text = "Next"
+                binding.btnBack.visibility = View.GONE
+
             }
-            // Create the AlertDialog
-            val alertDialog: AlertDialog = builder.create()
-            // Set other dialog properties
-            alertDialog.setCancelable(false)
-            alertDialog.show()
+            2 -> {
+                binding.accountDetails.visibility = View.VISIBLE
+                binding.mandateDetails.visibility = View.GONE
+                position = 1
+                binding.stepView.done(false)
+                binding.stepView.go(position, true)
+                binding.btnNext.text = "Next"
+                binding.btnBack.text = "Back"
+            }
         }
     }
 }
